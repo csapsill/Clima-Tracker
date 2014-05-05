27b9a39c86018951cf47160a301977e72020a573
devsil <devsil@hotmail.com>
2014-05-04 23:28:32 -0400

    JSON Handler and loading progress dialog screen.

--- Clima-Tracker/src/edu/purdue/lab6/JSON_Parse.java
+++ Clima-Tracker/src/edu/purdue/lab6/JSON_Parse.java
@@ -4,71 +4,137 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JSON_Parse {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class JSON_Parse extends AsyncTask<String,String,JSONObject>{
 	
-	
-	String HttpURL;
+	static InputStream inputStream;
+	static JSONObject jObj = null;
+	static String json ="";
+	Activity mAct;
+	Context mContext;
+	List<NameValuePair> list;
+	String method;
 	JSONObject jObject;
 	
+	ProgressDialog progDial;
 	// constructor
-	JSON_Parse(String url){
-		HttpURL = url;
+	JSON_Parse(Activity act,List<NameValuePair> list,Context context, String method){
+		this.mAct = act;
+		this.list = list;
+		this.method = method;
+		this.mContext = context;
 	}
 	
-	public JSONObject parse(){
-		DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
-		HttpPost httppost = new HttpPost(HttpURL);
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		progDial = new ProgressDialog(mContext);
+		progDial.setIndeterminate(false);
+		progDial.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progDial.setCancelable(true);
+		progDial.show();
 		
-		httppost.setHeader("Content-type","application/json");
-		
-		InputStream inputStream = null;
-		String result = null;
-		
-		try{
-			HttpResponse response = httpclient.execute(httppost);
-			HttpEntity entity = response.getEntity();
-			
-			inputStream = entity.getContent();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"),8);
-			StringBuilder sb = new StringBuilder();
-			
-			String line = null;
-			while((line = reader.readLine()) != null){
-				sb.append(line + "\n");
-			}
-			result = sb.toString();
-		} catch(Exception e){
-			
-		}
-		finally {
-			try{
-				if(inputStream != null) {
-					inputStream.close();
-				}
-			}catch (Exception e){
-				
-			}
-		}
-		
-		
-		try {
-			jObject = new JSONObject(result);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		
-		return jObject;
-				
 	}
+	
 
+	@Override
+	protected JSONObject doInBackground(String... params) {
+		//HttpResponse httpResponse = null;
+		try{
+			if(method == "POST"){
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpPost httpPost = new HttpPost(params[0]);
+				httpPost.setEntity(new UrlEncodedFormEntity(list));
+
+				HttpResponse httpResponse = httpClient.execute(httpPost);
+				StatusLine statusLine = httpResponse.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if(statusCode == 200){
+					HttpEntity httpEntity = httpResponse.getEntity();
+					inputStream = httpEntity.getContent();
+				}
+				else{
+					Log.e("LOG", "Failed to download result..");
+				}
+
+				Header contentencoding = httpResponse.getFirstHeader("Content-Encoding");
+				if(contentencoding != null && contentencoding.getValue().equalsIgnoreCase("gzip")){
+					inputStream = new GZIPInputStream(inputStream);
+				}
+
+				BufferedReader mBufferedReader = new BufferedReader( new InputStreamReader(inputStream, "iso-8859-1"),8);
+				StringBuilder stringBuilder = new StringBuilder();
+				String line = null;
+				while((line = mBufferedReader.readLine()) != null ){
+					stringBuilder.append(line +"\n");
+				}
+				inputStream.close();
+				json = stringBuilder.toString();
+
+				jObj = new JSONObject(json);
+
+			}
+			/*
+			else if(method == "GET"){
+				
+				HttpClient httpClient = new DefaultHttpClient();
+				String paramString = URLEncodedUtils.format(list, "utf-8");
+				Dataurl+= "?" +paramString;
+				HttpGet httpGet = new HttpGet(Dataurl);
+				
+				httpResponse = httpClient.execute(httpGet);
+				StatusLine statusLine = httpResponse.getStatusLine();
+				if()
+				
+				HttpEntity httpEntity = httpResponse.getEntity();
+				inputStream = httpEntity.getContent();
+			}*/
+			else{
+				Log.e("Request Alert","Wrong Method Name");
+			}
+		}
+		catch(UnsupportedEncodingException e){
+			Log.e("UnsupportEncodingException", e.getMessage().toString());
+		} catch (ClientProtocolException e) {
+			Log.e("ClientProtocalException", e.getMessage().toString());
+		} catch (IOException e) {
+			Log.e("IOException",e.getMessage().toString());
+		} catch (JSONException e) {
+			Log.e("JSONException", e.getMessage().toString());
+		}
+
+		return jObj;
+
+	}
+	
+	@Override
+	protected void onPostExecute(JSONObject obj){
+		
+		
+		
+		progDial.dismiss();
+	}
 }
